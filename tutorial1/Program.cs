c94dec8ad3fd8bf8070759c87251e24294310a69
Heorhii Myronenko <s25954.pjwstk.edu.pl>
2026-03-16 19:07:38 +0100

    Added MyClass with Main method

---
+++ tutorial1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class MyClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
